d777d4e8a41a007410acd4872f76b7e91de7a3af
kingstokes <kingstokes@gmail.com>
2014-02-09 21:34:34 -0500

    Hello World in Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ * Hello World in Java.
+ * Author: Robert Stokes
+ * 
+ */
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+
+	}
+
+}
